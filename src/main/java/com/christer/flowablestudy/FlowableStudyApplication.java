88cc0ef3ec60948b5bd2bafda918c58b3c5e2a4f
Christer <hongweibing3@gmail.com>
2023-06-02 09:58:22 +0800

    Initial commit

---
+++ src/main/java/com/christer/flowablestudy/FlowableStudyApplication.java
@@ -0,0 +1,13 @@
+package com.christer.flowablestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowableStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowableStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/christer/flowablestudy/FlowableStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.christer.flowablestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowableStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
