3ea2526bf13df9a60f92b3c8d3f3ab45fe276ef6
Christer <hongweibin3@gmail.com>
2024-02-26 20:30:39 +0800

    fix: 解决flowable-event-registry 对 liquibase 的依赖导致 Spring Boot 启动错误

--- src/main/java/com/christer/flowablestudy/FlowableStudyApplication.java
+++ src/main/java/com/christer/flowablestudy/FlowableStudyApplication.java
@@ -1,13 +1,22 @@
 package com.christer.flowablestudy;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FlowableStudyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FlowableStudyApplication.class, args);
+    }
+
+    @Bean
+    public SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setShouldRun(false);
+        return liquibase;
     }
 
 }
