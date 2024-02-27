b28e52c06aab3b20cd0b4a907b0ec6660ded2d9b
Christer <hongweibin3@gmail.com>
2024-02-27 19:58:36 +0800

    feat: 启动第一个流程实例

--- src/test/java/com/christer/flowablestudy/FlowableStudyApplicationTests.java
+++ src/test/java/com/christer/flowablestudy/FlowableStudyApplicationTests.java
@@ -2,6 +2,7 @@ package com.christer.flowablestudy;
 
 import org.flowable.engine.ProcessEngine;
 import org.flowable.engine.RepositoryService;
+import org.flowable.engine.RuntimeService;
 import org.flowable.engine.repository.Deployment;
 import org.flowable.engine.repository.DeploymentBuilder;
 import org.junit.jupiter.api.Test;
@@ -18,16 +19,38 @@ class FlowableStudyApplicationTests {
     @Resource
     private RepositoryService repositoryService;
 
+
+    @Resource
+    private RuntimeService runtimeService;
+    /**
+     * 部署流程
+     */
     @Test
     void deployProcess() {
 //        RepositoryService repositoryService = processEngine.getRepositoryService();
         DeploymentBuilder deployment = repositoryService.createDeployment();
         Deployment deploy = deployment
                 .addClasspathResource("process/FirstFlow.bpmn20.xml")
-                .addClasspathResource("process/holiday-request.bpmn20.xml")
                 .name("第二个流程图")
                 .deploy(); // 部署的方法
         System.out.println(deploy.getId());
     }
+
+    /**
+     * 启动流程实例
+     */
+    @Test
+    void startFlow() {
+        // 在流程定义表中动态维护
+        final String processId = "FirstFlow:1:60774fc6-d561-11ee-a396-d0abd5b04905";
+        // 我们创建流程图的时候自定义的，注意保证唯一
+        final String processKey = "FirstFlow";
+        // 1.根据流程定义ID启动流程实例
+//        runtimeService.startProcessInstanceById(processId);
+        // 2.根据流程定义Key启动流程实例
+        runtimeService.startProcessInstanceByKey(processKey);
+    }
+
+
 
 }
